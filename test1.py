93cab6bd33586107349a0bcbfc6a0d7d3f72373b
Shaw Innes <shaw@immortal.net.au>
2018-11-24 11:07:26 +1000

    feat: Update reference to images

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ import numpy as np
 import argparse
 import cv2
 
-image_path = '/Volumes/GoogleDrive/My Drive/Boeing Hack/Topic 4 - Airborne Imagery Analysis/image1.png'
+image_path = 'images/image1.png'
 # Load an color image in grayscale
 img = cv2.imread(image_path, 0)
 
