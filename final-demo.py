ca57bc6aafde1a49804a0ed33813a468f220bafc
Shaw Innes <shaw@immortal.net.au>
2018-11-25 15:23:17 +1000

    feat: Add scrollbars

--- final-demo.py
+++ final-demo.py
@@ -25,27 +25,25 @@ def nothing(x):
 
 cv2.namedWindow('image', cv2.WINDOW_GUI_EXPANDED)
 
-cv2.createTrackbar('hl','image',int(255.0 * 0.189),255,nothing)
-cv2.createTrackbar('sl','image',int(255.0 * 0.858),255,nothing)
-cv2.createTrackbar('vl','image',int(255.0 * 0.683),255,nothing)
-cv2.createTrackbar('hh','image',int(255.0 * 0.378),255,nothing)
-cv2.createTrackbar('sh','image',int(255.0 * 1.000),255,nothing)
-cv2.createTrackbar('vh','image',int(255.0 * 1.000),255,nothing)
+cv2.createTrackbar('hl', 'image', 1, 255, nothing)
+cv2.createTrackbar('sl', 'image', 1, 255, nothing)
+cv2.createTrackbar('vl', 'image', 1, 255, nothing)
+cv2.createTrackbar('hh', 'image', 255, 255, nothing)
+cv2.createTrackbar('sh', 'image', 255, 255, nothing)
+cv2.createTrackbar('vh', 'image', 255, 255, nothing)
 
 def processYolo(frame):
 	return frame
 
 def processHouse(frame):
 	hsv = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)  # COLOR_BGR2HSV
+
 	objectsMask = cv2.inRange(hsv, (0, 200, 0), (255, 255, 0))
-
 	edged = cv2.Canny(objectsMask, 30, 150)
-	contours = cv2.findContours(edged.copy(), cv2.RETR_TREE,
-								cv2.CHAIN_APPROX_SIMPLE)
-	contours = contours[0] if imutils.is_cv2() else contours[1]
-	contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contours = contours[0] if imutils.is_cv3() else contours[1]
 
-	coins = frame.copy()
+	copy = frame.copy()
 	for (i, c) in enumerate(contours):
 		rect = cv2.boundingRect(c)
 
@@ -54,33 +52,43 @@ def processHouse(frame):
 			3] < 6:  # means that the cars/houses will detected, assuming approximate max size
 			continue
 
-		area = cv2.contourArea(c)
-
 		# check for cars
 		if rect[3] < 15 and rect[2] < 15:
 			x, y, w, h = rect
-			cv2.circle(coins, (x, y), ((w + h) / 2), (232, 140, 83), 2)
+			cv2.circle(copy, (x, y), int((w + h) / 2), (232, 140, 83), 2)
 
 		# check for houses
 		if rect[3] > 60:
 			x, y, w, h = rect
-			pasteHouse = cv2.resize(house, (w / 2, w / 2))
-			coins[y:y + w / 2, x:x + w / 2] = pasteHouse
+			pasteHouse = cv2.resize(house, (int(w / 2), int(w / 2)))
+			copy[y:y + int(w / 2), x:x + int(w / 2)] = pasteHouse
 
-	return frame
+	return copy
 
 def processInfrared(frame):
 	return cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
 
+def processROI(frame):
+	return frame
+
 def processEdges(frame):
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+	hl = cv2.getTrackbarPos('hl', 'image')
+	hh = cv2.getTrackbarPos('hh', 'image')
+	sl = cv2.getTrackbarPos('sl', 'image')
+	sh = cv2.getTrackbarPos('sh', 'image')
+	vl = cv2.getTrackbarPos('vl', 'image')
+	vh = cv2.getTrackbarPos('vh', 'image')
+
+	blurred = cv2.GaussianBlur(hsv, (11, 11), 0)
 	mask = cv2.inRange(blurred, (hl, sl, vl), (hh, sh, vh))
 	otherMask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
-	# show the output frame
 	bitwiseAnd = cv2.bitwise_and(frame, otherMask)
+
 	out = cv2.cvtColor(bitwiseAnd, cv2.COLOR_HSV2BGR)
-	return bitwiseAnd
+
+	return out
 
 def processHarris(frame):
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -99,53 +107,16 @@ while True:
 	if frame is None:
 		break
 
-	hl = cv2.getTrackbarPos('hl', 'image')
-	sl = cv2.getTrackbarPos('sl', 'image')
-	vl = cv2.getTrackbarPos('vl', 'image')
-	hh = cv2.getTrackbarPos('hh', 'image')
-	sh = cv2.getTrackbarPos('sh', 'image')
-	vh = cv2.getTrackbarPos('vh', 'image')
-
-	# if mode == '1':
-	# 	frame = processHarris(frame)
-	# elif mode == '2':
-	# 	frame = processInfrared(frame)
-	# elif mode == '3':
-	# 	frame = processEdges(frame)
-	# elif mode == '4':
-	# 	frame = processHouse(frame)
-
-	hsv = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)  # COLOR_BGR2HSV
-	objectsMask = cv2.inRange(hsv, (0, 200, 0), (255, 255, 0))
-
-	edged = cv2.Canny(objectsMask, 30, 150)
-	contours = cv2.findContours(edged.copy(), cv2.RETR_TREE,
-								cv2.CHAIN_APPROX_SIMPLE)
-	contours = contours[0] if imutils.is_cv2() else contours[1]
-	contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-	coins = frame.copy()
-	for (i, c) in enumerate(contours):
-		rect = cv2.boundingRect(c)
-
-		# exit loop if abnormal size is detected
-		if rect[2] > 100 or rect[3] > 100 or rect[2] < 6 or rect[
-			3] < 6:  # means that the cars/houses will detected, assuming approximate max size
-			continue
-
-		area = cv2.contourArea(c)
-
-		# check for cars
-		if rect[3] < 15 and rect[2] < 15:
-			x, y, w, h = rect
-			cv2.circle(coins, (x, y), int((w + h) / 2), (232, 140, 83), 2)
-
-		# check for houses
-		if rect[3] > 60:
-			x, y, w, h = rect
-			pasteHouse = cv2.resize(house, (int(w / 2), int(w / 2)))
-			coins[y:y + int(w / 2), x:x + int(w / 2)] = pasteHouse
-
+	if mode == '1':
+		frame = processHarris(frame)
+	elif mode == '2':
+		frame = processInfrared(frame)
+	elif mode == '3':
+		frame = processEdges(frame)
+	elif mode == '4':
+		frame = processHouse(frame)
+	elif mode == '5':
+		frame = processHouse(frame)
 
 	cv2.imshow('image', frame)
 
--- test7.py
+++ test7.py
@@ -30,12 +30,12 @@ def nothing(x):
 
 cv2.namedWindow('image')
 
-cv2.createTrackbar('hl','image',int(255.0 * 0.191),255,nothing)
-cv2.createTrackbar('hh','image',int(255.0 * 0.490),255,nothing)
-cv2.createTrackbar('sl','image',int(255.0 * 0.081),255,nothing)
+cv2.createTrackbar('hl','image',int(255.0 * 0.189),255,nothing)
+cv2.createTrackbar('hh','image',int(255.0 * 0.378),255,nothing)
+cv2.createTrackbar('sl','image',int(255.0 * 0.858),255,nothing)
 cv2.createTrackbar('sh','image',int(255.0 * 1.000),255,nothing)
-cv2.createTrackbar('vl','image',int(255.0 * 0.108),255,nothing)
-cv2.createTrackbar('vh','image',int(255.0 * 0.629),255,nothing)
+cv2.createTrackbar('vl','image',int(255.0 * 0.683),255,nothing)
+cv2.createTrackbar('vh','image',int(255.0 * 1.000),255,nothing)
 
 # loop over frames from the video file stream
 while True:
@@ -49,44 +49,18 @@ while True:
  hsv = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR) #COLOR_BGR2HSV
  objectsMask = cv2.inRange(hsv, (0,200,0), (255,255,0))
 
-
- # if we are supposed to be writing a video to disk, initialize
- # the writer
- if args["output"] is not None and writer is None:
-   fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-   writer = cv2.VideoWriter(args["output"], fourcc, 30,
-     (frame.shape[1], frame.shape[0]), True)
-
- # check to see if we should write the frame to disk
- if writer is not None:
-   writer.write(frame)
-
- hl = 0.189 #cv2.getTrackbarPos('hl', 'image')
- sl = 0.858 #cv2.getTrackbarPos('sl', 'image')
- vl = 0.683 #cv2.getTrackbarPos('vl', 'image')
- hh = 0.378#cv2.getTrackbarPos('hh', 'image')
- sh = 1.0 #cv2.getTrackbarPos('sh', 'image')
- vh = 1.0 #cv2.getTrackbarPos('vh', 'image')
-
-
-# test it out for mask and houses, purple mask, and then find green!
- #out = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
-
-
- #blurred = cv2.GaussianBlur(objectsMask2, (11, 11), 0)
- #mask = cv2.inRange(blurred, (hl, sl, vl), (hh, sh, vh))
- #otherMask = cv2.cvtColor(blurred,)
-
- # show the output frame
- #bitwiseAnd = cv2.bitwise_and(frame, blurred)
-
+ hl = cv2.getTrackbarPos('hl', 'image') / 255.0
+ hh = cv2.getTrackbarPos('hh', 'image') / 255.0
+ sl = cv2.getTrackbarPos('sl', 'image') / 255.0
+ sh = cv2.getTrackbarPos('sh', 'image') / 255.0
+ vl = cv2.getTrackbarPos('vl', 'image') / 255.0
+ vh = cv2.getTrackbarPos('vh', 'image') / 255.0
 
  edged = cv2.Canny(objectsMask, 30, 150)
  contours = cv2.findContours(edged.copy(), cv2.RETR_TREE,
  	cv2.CHAIN_APPROX_SIMPLE)
  contours = contours[0] if imutils.is_cv2() else contours[1]
- additionalMask, contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+ contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
  coins = frame.copy()
  for (i, c) in enumerate(contours):
@@ -101,13 +75,13 @@ while True:
    # check for cars
    if rect[3] < 15 and rect[2] < 15:
         x, y, w, h = rect
-        cv2.circle(coins, (x, y), ((w + h)/2), (232, 140, 83), 2)
+        cv2.circle(coins, (x, y), int((w + h)/2), (232, 140, 83), 2)
 
     # check for houses
    if rect[3] > 60:
        x, y, w, h = rect
-       pasteHouse = cv2.resize(house, (w/2, w/2))
-       coins[y:y+w/2, x:x+w/2] = pasteHouse
+       pasteHouse = cv2.resize(house, (int(w/2), int(w/2)))
+       coins[y:y+int(w/2), x:x+int(w/2)] = pasteHouse
 
 
  cv2.imshow('image', coins)
